3c401cb3a7967d81ad28f521d3593e9200c76939
Grace Cai <grfsdggfsd@sadff.com>
2016-12-18 18:55:20 -0800

    dadd title

---
+++ Title.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { AppRegistry, View, Text, StyleSheet } from 'react-native'
+
+class Title extends Component {
+
+
+  render() {
+
+    return (
+      <Text>
+        "Joyce is Stupid"
+      </Text>
+    )
+  }
+}
+
+
+module.exports = Title
