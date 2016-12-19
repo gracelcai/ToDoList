be178350a12737456197aad68ebfc232cbb08b29
Grace <gracecai@Graces-MacBook-Pro.local>
2016-12-18 18:52:10 -0800

    my first version of running

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,28 @@
-import App from './app'
-import { AppRegistry } from 'react-native'
+import React, { Component } from 'react'
+import { AppRegistry, View, Text, StyleSheet } from 'react-native'
+
+
+
+var Title = require("./Title.js")
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Title>
+        </Title>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
 
 AppRegistry.registerComponent('TodoList', () => App)
